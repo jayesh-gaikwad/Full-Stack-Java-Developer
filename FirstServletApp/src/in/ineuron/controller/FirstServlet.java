489599cb31728f4047cfd201265699f7436741f5
jayeshdg <Jayesh_dg@1997>
2022-12-29 00:32:44 +0530

    First Commit

---
+++ FirstServletApp/src/in/ineuron/controller/FirstServlet.java
@@ -0,0 +1,67 @@
+package in.ineuron.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+public class FirstServlet implements Servlet {
+
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+    	System.out.println("Servlet Instantition...");
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Servlet Initialization....");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("Servlet DeInstantition...");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.println("<html><head><title>Output</title></head>");
+		out.println("<body>");
+		out.println("<h1 style='color:red; text-align:center;'>Coding in Eclipse is easy</h1>");
+		out.println("</body>");
+		out.println("</html");	
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
